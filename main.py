c1ff325a6ad37cad7a76f3b695ac06ab6fd1ffa2
Manish Kandepalli <79335262+manish295@users.noreply.github.com>
2021-10-29 19:50:39 -0400

    Added files

---
+++ main.py
@@ -0,0 +1,34 @@
+from stores import Stores
+import traceback
+
+game1 = Stores("Assassin's Creed Unity")
+try:
+    print("Epic Games:", game1.epic_games())
+except:
+    traceback.print_exc()
+
+
+try:
+    print("Steam:", game1.steam())
+except:
+    traceback.print_exc()
+
+
+try:
+    print("Humble Store:", game1.humble_store())
+except:
+    traceback.print_exc()
+
+try: 
+    print("Ubisoft Store:", game1.ubi_store())
+
+except:
+    traceback.print_exc()
+
+try:
+    print("Fanatical: ", game1.fanatical())
+except:
+    traceback.print_exc()
+
+game1.shut_down()
+
